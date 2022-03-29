26f43d609ac475f133b80f5374a2a11eb3669858
ur4an <aminbayevsirojiddin@gmail.com>
2022-03-29 18:38:54 +0500

    bootstrap

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+
+
+const app = express()
+
+
+app.get('/registration', (req, res) => {
+    res.sendFile('index.html', {root: './pages'} )
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile('index1.html', {root: './pages'} )
+})
+
+
+app.listen(6969, () => {
+    console.log('Started.');    
+})
