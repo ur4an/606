52da5a99825e33fa954f22a54fccf3c18ddd5730
ur4an <aminbayevsirojiddin@gmail.com>
2022-03-31 19:02:07 +0500

    mas

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,10 @@ app.get('/home', (req, res) => {
     res.sendFile('index1.html', {root: './pages'} )
 })
 
+app.get('/signIn', (req, res) => {
+    res.sendFile('index2.html', {root: './pages'} )
+})
+
 
 app.listen(6969, () => {
     console.log('Started.');    
